e3a1090d8f03c8cf3ff10eb8167935dbc13383fa
brocha115 <108730339+brocha115@users.noreply.github.com>
2022-10-27 14:04:15 +0100

    Add files via upload

---
+++ lbft/Part1/str/ft_strlcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (dst[i] != '\0' && < (size - 1))
+		{	
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	while (src[i] != '\0')
+		i++;
+	return(i);
+}
+
